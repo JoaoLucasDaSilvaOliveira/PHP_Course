c64c11fda874c99eda1b646634af26f40b0e039d
João Lucas <jlso9866@gmail.com>
2025-02-07 21:31:54 -0300

    Entendendo como funciona o case em PHP

---
+++ Conceitos_Básicos/case_sensitive.php
@@ -0,0 +1,13 @@
+<?php
+
+//para instruções PHP não há distinção de case, mas para vars e etc tem distinção
+//echo = ECHO
+//$nome != $NOME
+    echo "Teste ";
+    echO "Teste 2 ";
+    ECHO "Teste 3 <br> Teste 4";
+                //<br> quebra a linha que nem HTML 
+                
+    //var
+    $nome = "João";
+    echo "<br> $nome";
